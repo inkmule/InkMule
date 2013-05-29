466e4636a5a0d7f45586fc2cabfafe749b84592a
Daniel Hostetler <d.p.hostetler@gmail.com>
2013-05-28 22:33:23 -0400

    mo' footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -5,6 +5,51 @@
 
 <div id="footer">
 	<div class="container">
-		<p class="muted"></p>
+		<div id="footer_nav" class="span3">
+			<h4>site navigation</h4>
+			<ul>
+				<li class=""><a href="/quote/create">get a quote</a></li>
+				<li class=""><a href="/about">what we do</a></li>
+				<li class=""><a href="/portfolio/all">examples of past work</a></li>
+				<li class=""><a href="/contact">contact us</a></li>
+			</ul>
+		</div>
+		
+		<div id="footer_social" class="span3">
+			<h4>say hi</h4>
+			<ul>
+				<li class=""><a href="https://www.facebook.com/pages/Inkmule/273839796082073">facebook</a></li>
+				<li class=""><a href="https://twitter.com/inkmule">twitter</a></li>
+				<li class=""><a href="http://instagram.com/inkmule">instagram</a></li>
+			</ul>
+		</div>
+		
+		<div id="footer_legal" class="span3">
+			<h4>legal stuff</h4>
+			<ul>
+				<li class=""><a href="/terms">terms & conditions</a></li>
+				<li class=""><a href="/info">general information</a></li>
+			</ul>
+		</div>
+		
+		<div id="footer_mailing_list" class="span2">
+			<h4>stay in touch</h4>
+			<div id="join_mailing_list">
+			
+				<!-- Begin MailChimp Signup Form -->
+		
+				<div id="mc_embed_signup">
+				<form 		action="http://statusbro.us6.list-manage.com/subscribe/post?u=6362d2672877920edf27cab1f&amp;id=c730f539c9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				</form>
+				</div>
+
+				<!--End mc_embed_signup-->
+			
+			</div>
+		</div>
+		
 	</div>
 </div>
